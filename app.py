b137ee4263b39667e8435f46f8abfa6d84840d12
ajrb93 <ajbuchanan93@gmail.com>
2026-02-17 20:51:40 -0500

    20260217_25

--- app.py
+++ app.py
@@ -127,11 +127,17 @@ def show_standings_table(df, fmt_dict):
     grid_options['suppressSizeToFit'] = False
     grid_options['rowHeight'] = 25
     grid_options['headerHeight'] = 30
-    grid_options['defaultColDef'] = {**grid_options.get('defaultColDef', {}),'headerComponentParams': {
-        'template': '<span style="font-size:9px; font-weight:bold;">$col.displayName</span>'}}
-    for col in grid_options['columnDefs']:
-        col['headerStyle'] = {'fontSize': '9px', 'fontWeight': 'bold'}
-
+    grid_options['defaultColDef'] = {
+        **grid_options.get('defaultColDef', {}),
+        'headerComponentParams': {
+            'template': '''<div class="ag-cell-label-container" role="presentation">
+                <div ref="eLabel" class="ag-header-cell-label" role="presentation">
+                    <span ref="eText" class="ag-header-cell-text" style="font-size:9px; font-weight:bold;"></span>
+                </div>
+            </div>'''
+        }
+    }
+    
     AgGrid(
         display_df,
         gridOptions=grid_options,
