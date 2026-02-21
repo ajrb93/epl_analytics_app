704867cded40383e3e3f3afdaf105623da0b042d
ajrb93 <ajbuchanan93@gmail.com>
2026-02-21 09:46:38 -0500

    20260221_08

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def plot_standings_table(standings_df):
     # --- ROWS ---
     n_teams = len(standings_df)
     top = 0.95
-    bottom_margin = 0.01
+    bottom_margin = 0.02
     total_height = top - bottom_margin
     space = total_height / n_teams
     i_loc = top - space / 2
