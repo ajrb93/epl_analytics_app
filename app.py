4698deffaaf3dced00ecdcd07049b7c4a0dab856
ajrb93 <ajbuchanan93@gmail.com>
2026-02-21 22:29:15 -0500

    20260221_51

--- app.py
+++ app.py
@@ -597,7 +597,7 @@ def create_player_mvps(player_stats,matches_df,selected_season,selected_end_date
 def create_mvp_figure(plot_df):
     mvps = plot_df.sort_values('MVPRtg',ascending=False).head(100)
 
-    fig, ax = plt.subplots(figsize=(7,35))
+    fig, ax = plt.subplots(figsize=(10,50))
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
     ax.axis('off')
