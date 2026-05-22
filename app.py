b06d28d916563d5075fef3b8262295a59a11e8a0
ajrb93 <ajbuchanan93@gmail.com>
2026-05-21 21:38:49 -0400

    20260521

--- app.py
+++ app.py
@@ -925,7 +925,7 @@ def plot_spi_chart(data):
     fig.add_hline(y=0.5, line_dash='dash', line_color='gray', line_width=2)
     for year in pd.to_datetime(data.Date).dt.year.unique():
         fig.add_vline(x=pd.Timestamp(f'{year}-08-01').timestamp()*1000,line_dash='dot', line_color='black', line_width=2)
-    fig.update_layout(height=105, margin=dict(l=0, r=0, t=0, b=0),yaxis=dict(range=[0.20, 0.8], tickvals=[i/10 for i in range(3, 8)],tickformat='.0%'),
+    fig.update_layout(height=105, margin=dict(l=0, r=0, t=0, b=0),yaxis=dict(range=[0.05, 0.95], tickvals=[i/10 for i in range(1, 10, 2)],tickformat='.0%'),
                       plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
     season_start = '08-01'
     season_end = '06-01'
@@ -1185,7 +1185,7 @@ def plot_player_heatmaps(df,selected_team,selected_season):
                     ax.add_patch(plt.Rectangle((j+names+(1-names)/n_games*minutes_start_val/90,k),(1-names)/n_games*minutes_val/90,-(header)/players,color=box_color,
                                 ec='#444444',lw=0.5))
                     if minutes_val > 0:
-                        ax.annotate(np.round(rtg_val,1),(j+names+(1-names)/n_games/2,k-(header)/players/2),ha='center',va='center')
+                        ax.annotate(str(np.round(rtg_val,1)).fillna(''),(j+names+(1-names)/n_games/2,k-(header)/players/2),ha='center',va='center')
             j += (1-names)/n_games
         k += (header)/players    
     plt.tight_layout()
